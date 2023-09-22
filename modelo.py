b8699a837c7b501b429b5f17b2a85b7a7b64ac12
Betops93 <adalbertops93@live.com>
2023-09-22 19:41:03 -0300

    aula05

--- modelo.py
+++ modelo.py
@@ -1,6 +1,3 @@
-import ply
-
-
 class Programa:
     def __init__(self, nome, ano):
         self._nome = nome.title()
@@ -49,12 +46,14 @@ class Playlist:
         self.nome = nome
         self._programas = programas
 
+    def __getitem__(self, item):
+        return self._programas[item]
+
     @property
     def listagem(self):
         return self._programas
-    
-    @property
-    def tamanho(self):
+
+    def __len__(self):
         return len(self._programas)
 
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
@@ -76,10 +75,14 @@ filmes_e_series = [vingadores, atlanta, demolidor, tmp]
 
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-print(f'Tamanho do playlist: {len(playlist_fim_de_semana.listagem)}')
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
 
-for programa in playlist_fim_de_semana.listagem:
+for programa in playlist_fim_de_semana:
     print(programa)
 
-print(demolidor in playlist_fim_de_semana.listagem)
+
 print('Fim do Código')
+print(len(playlist_fim_de_semana))
+print(demolidor in playlist_fim_de_semana.listagem)
+print(vingadores in playlist_fim_de_semana)
+print(playlist_fim_de_semana[0])
